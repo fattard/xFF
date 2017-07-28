8c72af3f4299e0f7dbcfaa4524310a005aed962b
fattard <fattard.dev@gmail.com>
2017-07-28 02:15:45 -0300

    [Unity3D] Helper classes to allow dynamic runtime determination of which core to launch when opening app

---
+++ unity3D_proj/Assets/Scripts/EmuEnvironment.cs
@@ -0,0 +1,81 @@
+﻿/*
+*   This file is part of xFF
+*   Copyright (C) 2017 Fabio Attard
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*   Additional Terms 7.b and 7.c of GPLv3 apply to this file:
+*       * Requiring preservation of specified reasonable legal notices or
+*         author attributions in that material or in the Appropriate Legal
+*         Notices displayed by works containing it.
+*       * Prohibiting misrepresentation of the origin of that material,
+*         or requiring that modified versions of such material be marked in
+*         reasonable ways as different from the original version.
+*/
+
+using UnityEngine;
+using xFF.Frontend.Unity3D.BytePusher;
+
+namespace xFF
+{
+
+
+    public class EmuEnvironment : MonoBehaviour
+    {
+        public enum Cores
+        {
+            _Unknown,
+
+            BytePusher,
+            GB,           
+        }
+
+
+        public static Cores EmuCore
+        {
+            get;
+            set;
+        }
+
+
+        public static string RomFilePath
+        {
+            get;
+            set;
+        }
+
+
+
+        static EmuEnvironment( )
+        {
+            EmuCore = Cores._Unknown;
+            RomFilePath = string.Empty;
+        }
+
+
+
+        void Awake( )
+        {
+            switch (EmuCore)
+            {
+                case Cores.BytePusher:
+                    FrontendBytePusher.ConfigScene();
+                    break;
+            }
+        }
+    }
+
+
+}
+// namespace xFF
