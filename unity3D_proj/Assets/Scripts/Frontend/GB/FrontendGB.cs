14a3fe0b0dc57f95e23fe948baeb3f775d19435f
fattard <fattard.dev@gmail.com>
2017-08-06 00:56:45 -0300

    [Unity3D] Better integration with EmuGB Added a check to pause Unity from unresponsive loops (happens when printing lots of logs)

--- unity3D_proj/Assets/Scripts/Frontend/GB/FrontendGB.cs
+++ unity3D_proj/Assets/Scripts/Frontend/GB/FrontendGB.cs
@@ -47,28 +47,88 @@ namespace xFF
 
                     EmuCores.GB.EmuGB m_emuGB;
 
+                    float lastUpdateTick;
+
 
                     void Start( )
                     {
-                        EmuCores.GB.ConfigsGB configGB = new EmuCores.GB.ConfigsGB();
+                        EmuCores.GB.ConfigsGB configsGB = LoadConfigFile();
 
-                        // Bootrom
-                        configGB.bootRomDMG.enabled = true;
-                        configGB.bootRomDMG.path = frontendConfigs.bootRomPath;
+                        // Running direct from scene
+                        if (EmuEnvironment.EmuCore == EmuEnvironment.Cores._Unknown)
+                        {
+                            EmuEnvironment.RomFilePath = frontendConfigs.romsPath[frontendConfigs.selectedRomIndex];
 
-                        // DMG Colors
-                        configGB.dmgColors.color0.Set((int)(255 * frontendConfigs.color0.r), (int)(255 * frontendConfigs.color0.g), (int)(255 * frontendConfigs.color0.b));
-                        configGB.dmgColors.color1.Set((int)(255 * frontendConfigs.color1.r), (int)(255 * frontendConfigs.color1.g), (int)(255 * frontendConfigs.color1.b));
-                        configGB.dmgColors.color2.Set((int)(255 * frontendConfigs.color2.r), (int)(255 * frontendConfigs.color2.g), (int)(255 * frontendConfigs.color2.b));
-                        configGB.dmgColors.color3.Set((int)(255 * frontendConfigs.color3.r), (int)(255 * frontendConfigs.color3.g), (int)(255 * frontendConfigs.color3.b));
-                        configGB.dmgColors.colorDisabledLCD.Set((int)(255 * frontendConfigs.color0.r), (int)(255 * frontendConfigs.color0.g), (int)(255 * frontendConfigs.color0.b));
+                            OverrideConfigsWithFrontend(configsGB);
+                        }
 
                         gbDisplay.displayWidth = 160;
                         gbDisplay.displayHeight = 144;
-                        gbDisplay.displayZoomFactor = frontendConfigs.zoomFactor;
+                        gbDisplay.displayZoomFactor = configsGB.graphics.displayZoom;
                         gbDisplay.SetScreenStandard();
 
-                        m_emuGB = new EmuCores.GB.EmuGB(configGB);
+                        m_emuGB = new EmuCores.GB.EmuGB(configsGB);
+                        //m_emuGB.DrawDisplay = DisplayRenderer;
+                        //m_emuGB.PlayAudio = PlayAudio;
+                        //m_emuGB.UpdateInputKeys = UpdateKeys;
+
+                        LoadBootRom();
+                        LoadROM();
+                        
+                        SaveConfigFile(configsGB);
+
+                        lastUpdateTick = Time.realtimeSinceStartup;
+                    }
+
+
+                    void LoadBootRom( )
+                    {
+                        // Use internal boot rom
+                        if (!m_emuGB.Configs.bootRomDMG.enabled || string.IsNullOrEmpty(m_emuGB.Configs.bootRomDMG.path))
+                        {
+                            byte[] devBootROM = Resources.Load<TextAsset>("GB/DMG_CustomBootRom").bytes;
+
+                            m_emuGB.SetBootRom(devBootROM);
+                        }
+
+                        else
+                        {
+                            try
+                            {
+                                byte[] bootROM = System.IO.File.ReadAllBytes(m_emuGB.Configs.bootRomDMG.path);
+
+                                if (bootROM.Length != 0x100) // 256 bytes
+                                {
+                                    throw new System.ArgumentException("Invalid BootROM format. Expected " + 0x100 + " bytes.\nFound " + bootROM.Length + ". File: " + m_emuGB.Configs.bootRomDMG.path);
+                                }
+
+                                m_emuGB.SetBootRom(bootROM);
+                            }
+                            catch (System.Exception e)
+                            {
+                                Debug.LogError("BootROM error - " + e.Message);
+                            }
+                        }
+                    }
+
+
+                    void LoadROM()
+                    {
+                        try
+                        {
+                            string path = EmuEnvironment.RomFilePath;
+
+                            byte[] romData = System.IO.File.ReadAllBytes(path);
+
+                            if (!m_emuGB.LoadSimpleRom(romData))
+                            {
+                                throw new System.ArgumentException("Invalid ROM format");
+                            }
+                        }
+                        catch (System.Exception e)
+                        {
+                            Debug.LogError("Failed loading rom: " + e.Message);
+                        }
                     }
 
 
@@ -85,6 +145,72 @@ namespace xFF
                         }
 
                         gbDisplay.DrawDisplay(displayPixels);
+
+                        // Prevent DBG unresponsive loops
+                        float newTime = Time.realtimeSinceStartup;
+                        if ((newTime - lastUpdateTick) > 5)
+                        {
+                            Debug.LogWarning("Breaking due to long loop last time.");
+                            Debug.Break();
+                        }
+                        lastUpdateTick = newTime;
+                    }
+
+
+                    EmuCores.GB.ConfigsGB LoadConfigFile( )
+                    {
+                        try
+                        {
+                            string confFile = System.IO.File.ReadAllText(Application.dataPath + "/../core_GB.cfg");
+
+                            EmuCores.GB.ConfigsGB confData = JsonUtility.FromJson<EmuCores.GB.ConfigsGB>(confFile);
+
+                            return confData;
+                        }
+                        catch (System.Exception e)
+                        {
+                            return new EmuCores.GB.ConfigsGB();
+                        }
+                    }
+
+
+                    void SaveConfigFile(EmuCores.GB.ConfigsGB aConfigs)
+                    {
+                        try
+                        {
+                            string confFile = JsonUtility.ToJson(aConfigs, prettyPrint: true);
+
+                            System.IO.File.WriteAllText(Application.dataPath + "/../core_GB.cfg", confFile);
+                        }
+                        catch (System.Exception e)
+                        {
+                            Debug.LogError(e.Message);
+                        }
+                    }
+
+
+                    void OverrideConfigsWithFrontend(EmuCores.GB.ConfigsGB aConf)
+                    {
+                        // Graphics
+                        aConf.graphics.displayZoom = frontendConfigs.zoomFactor;
+
+                        // Bootrom
+                        aConf.bootRomDMG.enabled = false;
+                        aConf.bootRomDMG.path = frontendConfigs.bootRomPath;
+
+                        // DMG Colors
+                        aConf.dmgColors.color0.Set((int)(255 * frontendConfigs.color0.r), (int)(255 * frontendConfigs.color0.g), (int)(255 * frontendConfigs.color0.b));
+                        aConf.dmgColors.color1.Set((int)(255 * frontendConfigs.color1.r), (int)(255 * frontendConfigs.color1.g), (int)(255 * frontendConfigs.color1.b));
+                        aConf.dmgColors.color2.Set((int)(255 * frontendConfigs.color2.r), (int)(255 * frontendConfigs.color2.g), (int)(255 * frontendConfigs.color2.b));
+                        aConf.dmgColors.color3.Set((int)(255 * frontendConfigs.color3.r), (int)(255 * frontendConfigs.color3.g), (int)(255 * frontendConfigs.color3.b));
+                        aConf.dmgColors.colorDisabledLCD.Set((int)(255 * frontendConfigs.color0.r), (int)(255 * frontendConfigs.color0.g), (int)(255 * frontendConfigs.color0.b));
+                    }
+
+
+                    public static void ConfigScene()
+                    {
+                        GameObject go = Resources.Load<GameObject>("EmuGB");
+                        Instantiate(go);
                     }
                 }
 
