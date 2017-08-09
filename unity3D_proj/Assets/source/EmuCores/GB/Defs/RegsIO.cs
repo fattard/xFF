3c24d322fc375cc25c61a4665a175a7750499d7f
fattard <fattard.dev@gmail.com>
2017-08-09 02:04:15 -0300

    [DMG] Added defs for interrupts (IF and IE) I/O Registers

--- unity3D_proj/Assets/source/EmuCores/GB/Defs/RegsIO.cs
+++ unity3D_proj/Assets/source/EmuCores/GB/Defs/RegsIO.cs
@@ -39,7 +39,10 @@ namespace xFF
                 /// </summary>
                 public static class RegsIO
                 {
-                    public const int P1   = 0xFF00;  // Joystick Port
+                    public const int P1   = 0xFF00;  // Joypad Ports
+
+                    public const int IF   = 0xFF0F;  // Interrupt Request
+                    public const int IE   = 0xFFFF;  // Interrupt Enable
 
                     public const int SCY  = 0xFF42;  // Background ScrollY
                     public const int SCX  = 0xFF43;  // Background ScrollX
@@ -49,7 +52,6 @@ namespace xFF
                     public const int BGP  = 0xFF47;  // DMG Background Palette
 
                     public const int BOOT = 0xFF50;  // BootROM protection
-
                 }
 
 
@@ -60,16 +62,30 @@ namespace xFF
                 public static class RegsIO_Bits
                 {
                     // P1
-                    public const int P10 = (1 << 0);
-                    public const int P11 = (1 << 1);
-                    public const int P12 = (1 << 2);
-                    public const int P13 = (1 << 3);
-                    public const int P14 = (1 << 4);
-                    public const int P15 = (1 << 5);
+                    public const int P10 = (1 << 0); // Input Port P10
+                    public const int P11 = (1 << 1); // Input Port P11
+                    public const int P12 = (1 << 2); // Input Port P12
+                    public const int P13 = (1 << 3); // Input Port P13
+                    public const int P14 = (1 << 4); // Output Port P14
+                    public const int P15 = (1 << 5); // Output Port P15
+
+                    // IF
+                    public const int IF_VBLANK = (1 << 0); // Vertical Blanking
+                    public const int IF_STAT   = (1 << 1); // LCDC (STAT referenced)
+                    public const int IF_TIMER  = (1 << 2); // Timer overflow
+                    public const int IF_SERIAL = (1 << 3); // Serial I/O transfer completion
+                    public const int IF_JOYPAD = (1 << 4); // P10-P13 terminal negative edge
+
+
+                    // IE
+                    public const int IE_VBLANK = (1 << 0); // Vertical Blanking
+                    public const int IE_STAT   = (1 << 1); // LCDC (STAT referenced)
+                    public const int IE_TIMER  = (1 << 2); // Timer overflow
+                    public const int IE_SERIAL = (1 << 3); // Serial I/O transfer completion
+                    public const int IE_JOYPAD = (1 << 4); // P10-P13 terminal negative edge
 
                     // BOOT
                     public const int BOOT_LOCK = (1 << 0);
-
                 }
 
 
