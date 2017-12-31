e34d8b32efc1c77ed8799f1cc12d77792794085c
fattard <fattard.dev@gmail.com>
2017-12-31 15:29:30 -0200

    [DMG] Added new feature to DMG_CustomBootRom to allow picking 3 different animation type based on compile time or by patching a data offset in memory: - No Animation (instant boot); - Quick Animation (1 sec show logo and play sounds animation, based on SameBoy bootrom source); - Full Animation (6 sec animation, scrolls logo to center and play sounds, based on the original bootrom).
    Would like to trap invalid ROMs in the Full Animation, but space now is very tightly with only 5 free bytes remaining

--- CutomBootROM/DMG/DMG_CustomBootRom.asm
+++ CutomBootROM/DMG/DMG_CustomBootRom.asm
@@ -35,12 +35,25 @@
 .rombanksize $100
 .rombanks 1
 
-; Target: total ticks 23440324
+; Original boot:  23440324 total ticks
+; Current Custom: 25614188 total ticks
 
 
-; Offset for Cart Header: ROM Size
-.define VALID_CHECK_OFFSET $0148
+; Offsets
+.define VALID_CHECK_OFFSET      $0148 ; for Cart Header: ROM Size
+.define LOGO_DATA_OFFSET        $0104 ; for the Logo stored in cart header
+.define TRADEMARK_SYMBOL_OFFSET $00F5 ; for the TrademarkSymbol data
+.define ANIM_TYPE_OFFSET        $00FD ; for the AnimType data
 
+
+; Custom Boot anim type - can be patched in memory at ANIM_TYPE_OFFSET
+.define CUSTOM_BOOT_TYPE_NO_ANIM        $00 ; instant boot
+.define CUSTOM_BOOT_TYPE_QUICK_ANIM     $03 ; 1 sec anim, showing logo and playing sounds
+.define CUSTOM_BOOT_TYPE_FULL_ANIM      $AA ; 6 sec anim, scrolls logo to center and play sounds
+
+
+.define FORCE_FULL_ANIM
+.define FORCE_QUICK_ANIM
 
 
 ; ---------- Code Init ----------
@@ -50,6 +63,7 @@ Start:
     ld sp, $FFFE
 
 ; Clear memory VRAM - ($8000-$9FFF)
+ClearVRAM:
     xor a
     ld hl, VRAM
 clearVRAMLoop
@@ -58,43 +72,107 @@ clearVRAMLoop
     jr z, clearVRAMLoop
     
 ; Setup audio
+SetupAudio:
     ld a, $80
     ldh [<rNR52], a     ; [$FF26] = $80 : Turn on audio
     ldh [<rNR11], a     ; [$FF11] = $80 : Channel 1 wave duty
     ld a, $F3
     ldh [<rNR12], a     ; [$FF12] = $F3 : Channel 1 envelope
-    ldh [<rNR51], a     ; [$FF25] = $F3 : Channel routing
-    ld a, $C1
-    ldh [<rNR13], a     ; [$FF13] = $C1 : Channel 1 low frequency byte
+    ldh [<rNR51], a     ; [$FF25] = $F3 : Sound output routing
     ld a, $77
     ldh [<rNR50], a     ; [$FF24] = $77 : Master volume
     
 ; Setup BG Palette
+SetupBGP:
     ld a, $FC
     ldh [<rBGP], a      ; [$FF47] = MakePal(0, 3, 3, 3)
     
-    
-    
+        
 ; Check for Inserted Cartridge
+CheckInvalidROM:
     ld de, VALID_CHECK_OFFSET
     ld a, [DE]          ; When there are no inserted cartridge, this area reads as $FF (Header: ROM size)
     cp $FF              
     jr z, InvalidROMInserted ; Invalid ROM found
+
+    
+; Check for Custom Boot Anim Type
+CheckCustomAnimType:
+    ld de, ANIM_TYPE_OFFSET
+    ld a, [DE]          ; Read the desired anim type 
+    cp CUSTOM_BOOT_TYPE_NO_ANIM
+    jr z, BootType_NoAnim
+    push af ; stores custom boot anim type
+    
+    
+; Routine to copy the logo data to the VRAM
+LoadLogoToVRAM:
+    ; A nibble represents a 4-pixels line, 2 bytes represent a 4x4 tile, scaled to 8x8.
+    ; Tiles are ordered left to right, top to bottom.
+    ld de, LOGO_DATA_OFFSET ; Logo start
+    ld hl, VRAM+$10         ; Copy starting from second tile in VRAM
+loadLogoLoop
+    ld a, [de] ; Read 2 rows
+    ld b, a
+    call DoubleBitsAndWriteRow
+    call DoubleBitsAndWriteRow
     inc de
-    ld a, [DE]          ; When there are no inserted cartridge, this area reads as $FF (Header: SRAM size)
-    cp $FF
-    jr z, InvalidROMInserted ; Invalid ROM found
-    jr FinishSetup
-    
-InvalidROMInserted
+    ld a, e
+    xor $34 ; End of logo
+    jr nz, loadLogoLoop
+    ; Load trademark symbol
+    ld de, TRADEMARK_SYMBOL_OFFSET
+    ld c,$08
+loadTrademarkSymbolLoop:
+    ld a,[de]
+    inc de
+    ldi [hl],a
+    inc hl
+    dec c
+    jr nz, loadTrademarkSymbolLoop
+    ; Set up tilemap
+    ld a,$19      ; Trademark symbol
+    ld [$9910], a ; ... put in the superscript position
+    ld hl,$992F   ; Bottom right corner of the logo
+    ld c,$0C      ; Tiles in a logo row
+tilemapLoop
+    dec a
+    jr z, tilemapDone
+    ldd [hl], a
+    dec c
+    jr nz, tilemapLoop
+    ld l,$0F ; Jump to top row
+    jr tilemapLoop
+tilemapDone
+
+; Set vertical scroll register
+    ld a, $64
+    ldh [<rSCY], a    
     call TurnOnLCD
-bootTrapped
-    jr bootTrapped
     
+    pop af ; restore custom boot anim type
+    cp CUSTOM_BOOT_TYPE_QUICK_ANIM
+    jr z, BootType_QuickAnim ; skip to quick part
     
+ScrollLogo:
+    ld b, 3
+    call WaitBFrames
+    ldh a, [<rSCY]
+    dec a
+    ldh [<rSCY], a
+    jr nz, ScrollLogo
     
-FinishSetup  
+BootType_QuickAnim
+    xor a
+    ldh [<rSCY], a  ; forces vertical scroll to zero
+    call ShowLogoAndPlayAudioAnim
+    jr FinishSetup 
+   
+ 
+BootType_NoAnim
     call TurnOnLCD
+    
+FinishSetup:
 ; Set registers to match the original DMG boot
     ld hl, $01B0
     push hl
@@ -104,31 +182,118 @@ FinishSetup
     ld de, $00D8
     
 ; Ends boot process and starts game
-    jp BootEnd
+    jr BootEnd
     
+
     
+; Invalid ROM size was read, must trap here
+InvalidROMInserted
+    call TurnOnLCD
+bootTrapped
+    jr bootTrapped      ; ------ traps execution here ------
     
+
 
 ; Routine to turn LCD on and wait for a VBlank
 TurnOnLCD:
 ; Init LCD
     ld a, $91
-    ldh [<rLCDC], a     ; [$FF40] = (LCD_EN | BG_EN | CHAR_OFF_0)  : Turn on LCD
-    
+    ldh [<rLCDC], a     ; [$FF40] = (LCD_EN | BG_EN | CHAR_ADDR_0) : Turn on LCD
 ; Wait for VBlank
-_wait_ly_143            ; wait for LY = 143 first to ensure a fresh vblank
-    ldh a, (<rLY)
+    call WaitFrame
+    ret
+    
+    
+    
+; Routine to show Logo and play sounds
+ShowLogoAndPlayAudioAnim:
+; Wait ~0.75 seconds
+    ld b, 5
+    call WaitBFrames
+
+    ; Play first sound
+    ld a, $83
+    call PlaySound
+    ld b, 5
+    call WaitBFrames
+    ; Play second sound
+    ld a, $C1
+    call PlaySound
+
+; Wait ~0.9 seconds
+    ld b, 50
+    call WaitBFrames
+    ret
+    
+
+    
+; Routine to expand and scale Logo data into VRAM 
+; by doubling the most significant 4 bits, then b is shifted by 4
+DoubleBitsAndWriteRow:
+    ld a, 4
+    ld c, 0
+doubleCurrentBit
+    sla b
+    push af
+    rl c
+    pop af
+    rl c
+    dec a
+    jr nz, doubleCurrentBit
+    ld a, c
+; Write as two rows
+    ldi [hl], a
+    inc hl
+    ldi [hl], a
+    inc hl
+    ret
+    
+    
+    
+; Routine to wait for a frame
+WaitFrame:
+    ldh a, [<rLY]
     cp 143
-    jr nz, _wait_ly_143
-_wait_ly_144            ; wait for LY = 144
-    ldh a, (<rLY)
+    jr nz, WaitFrame
+wait_ly_144
+    ldh a, [<rLY]
     cp 144
-    jr nz, _wait_ly_144
+    jr nz, wait_ly_144
     ret
 
+    
+; Routine to wait a specific number of frames
+WaitBFrames:
+    call WaitFrame
+    dec b
+    jr nz, WaitBFrames
+    ret
 
+    
+; Routine to play the logo tones
+PlaySound:
+    ldh [<rNR13], a
+    ld a, $87
+    ldh [<rNR14], a
+    ret
+    
+    
+.org TRADEMARK_SYMBOL_OFFSET
+    .db $3C,$42,$B9,$A5,$B9,$A5,$42,$3C
+    
+.org ANIM_TYPE_OFFSET
+; Compile definition of hardcoded boot type anim data
+.ifdef FORCE_FULL_ANIM
+    .db CUSTOM_BOOT_TYPE_FULL_ANIM
+.else .ifdef FORCE_QUICK_ANIM  
+    .db CUSTOM_BOOT_TYPE_QUICK_ANIM
+.else
+    .db CUSTOM_BOOT_TYPE_NO_ANIM
+.endif
+.endif
 
+    
+; ---------- Last code ----------
 .org $00FE
-
-BootEnd:
-    ldh [<rBOOT], a   ; Turn off BootROM and enables ROMBank 0 access     
+BootEnd
+    ldh [<rBOOT], a   ; Turn off BootROM and enables IRQ vectors access     
